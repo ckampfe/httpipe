fc25b9c69059e7e700dbeaa7d25495d836257025
Clark Kampfe <clark.kampfe@gmail.com>
2025-02-02 22:52:25 -0600

    add named autovivify for channels

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
 #![forbid(unsafe_code)]
+// TODO
+// - [ ] named topics
+// - [ ] list topics
+// - [x] option to autovivify channels
+// - [ ] option to autivivify pubsubs
+// - [x] option on startup whether to allow autovivify or not
+// - [ ] namespaces, e.g., /channels/some_namespace/some_id
+// - [ ] reevalute API endpoints to be more RESTish
+// - [ ] GET only API for browser stuff
 
 use axum::body::{Body, BodyDataStream, Bytes};
 use axum::extract::{Path, State};
@@ -151,19 +160,31 @@ async fn pubsub_count(
     }
 }
 
+async fn channel_index(
+    State(state): State<Arc<AppState>>,
+) -> axum::response::Result<axum::Json<Vec<String>>> {
+    let channel_clients = state.channel_clients.lock().await;
+
+    Ok(axum::Json(
+        channel_clients.keys().cloned().collect::<Vec<_>>(),
+    ))
+}
+
 async fn channel_create(
     State(state): State<Arc<AppState>>,
 ) -> axum::response::Result<impl IntoResponse> {
     let mut channel_clients = state.channel_clients.lock().await;
 
-    let channel_id = Uuid::new_v4();
+    let channel_id = Uuid::new_v4().to_string();
 
-    if let std::collections::hash_map::Entry::Vacant(e) = channel_clients.entry(channel_id) {
+    if let std::collections::hash_map::Entry::Vacant(e) =
+        channel_clients.entry(channel_id.to_string())
+    {
         let (tx, rx) = flume::bounded(0);
 
         e.insert((tx, rx));
 
-        Ok((StatusCode::CREATED, channel_id.to_string()))
+        Ok((StatusCode::CREATED, channel_id))
     } else {
         Err(StatusCode::INTERNAL_SERVER_ERROR.into())
     }
@@ -171,33 +192,39 @@ async fn channel_create(
 
 async fn channel_broadcast(
     request_headers: HeaderMap,
-    Path(id): Path<Uuid>,
+    Path(id): Path<String>,
     State(state): State<Arc<AppState>>,
     body: Body,
 ) -> axum::response::Result<()> {
-    let channel_clients = state.channel_clients.lock().await;
+    let mut channel_clients = state.channel_clients.lock().await;
 
-    if let Some((tx, _rx)) = channel_clients.get(&id) {
-        let tx = tx.clone();
+    let tx = if let Some((tx, _rx)) = channel_clients.get(&id) {
+        tx.clone()
+    } else if state.options.autovivify {
+        let (tx, rx) = flume::bounded(0);
 
-        drop(channel_clients);
+        channel_clients.insert(id, (tx.clone(), rx));
 
-        let body_stream = body.into_data_stream();
-
-        let (done, done_rx) = Done::new();
-
-        tx.send_async((body_stream, request_headers, done))
-            .await
-            .map_err(|_e| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-        done_rx
-            .await
-            .map_err(|_e| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-        Ok(())
+        tx
     } else {
-        Err(StatusCode::NOT_FOUND.into())
-    }
+        return Err(StatusCode::NOT_FOUND.into());
+    };
+
+    drop(channel_clients);
+
+    let body_stream = body.into_data_stream();
+
+    let (done, done_rx) = Done::new();
+
+    tx.send_async((body_stream, request_headers, done))
+        .await
+        .map_err(|_e| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    done_rx
+        .await
+        .map_err(|_e| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok(())
 }
 
 /// when this is dropped it signals the oneshot channel
@@ -224,47 +251,53 @@ impl Drop for Done {
 }
 
 async fn channel_subscribe(
-    Path(id): Path<Uuid>,
+    Path(id): Path<String>,
     State(state): State<Arc<AppState>>,
 ) -> axum::response::Result<impl IntoResponse> {
-    let channel_clients = state.channel_clients.lock().await;
+    let mut channel_clients = state.channel_clients.lock().await;
 
-    if let Some((_tx, rx)) = channel_clients.get(&id) {
-        let rx = rx.clone();
+    let rx = if let Some((_tx, rx)) = channel_clients.get(&id) {
+        rx.clone()
+    } else if state.options.autovivify {
+        let (tx, rx) = flume::bounded(0);
 
-        drop(channel_clients);
+        channel_clients.insert(id, (tx, rx.clone()));
 
-        let rx = rx.into_recv_async();
-
-        let (stream, producer_request_headers, _done) =
-            rx.await.map_err(|_e| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-        let body = Body::from_stream(stream);
-
-        // we do this because by default, POSTs from curl are `x-www-form-urlencoded`
-        let producer_content_type = producer_request_headers
-            .get(header::CONTENT_TYPE)
-            .cloned()
-            .and_then(|header_value| {
-                // TODO should we also reject multipart/form-data?
-                if header_value == "application/x-www-form-urlencoded" {
-                    None
-                } else {
-                    Some(header_value)
-                }
-            })
-            .unwrap_or(HeaderValue::from_static("application/octet-stream"));
-
-        let headers = [(header::CONTENT_TYPE, producer_content_type)];
-
-        Ok((headers, body).into_response())
+        rx
     } else {
-        Err(StatusCode::NOT_FOUND.into())
-    }
+        return Err(StatusCode::NOT_FOUND.into());
+    };
+
+    drop(channel_clients);
+
+    let rx = rx.into_recv_async();
+
+    let (stream, producer_request_headers, _done) =
+        rx.await.map_err(|_e| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let body = Body::from_stream(stream);
+
+    // we do this because by default, POSTs from curl are `x-www-form-urlencoded`
+    let producer_content_type = producer_request_headers
+        .get(header::CONTENT_TYPE)
+        .cloned()
+        .and_then(|header_value| {
+            // TODO should we also reject multipart/form-data?
+            if header_value == "application/x-www-form-urlencoded" {
+                None
+            } else {
+                Some(header_value)
+            }
+        })
+        .unwrap_or(HeaderValue::from_static("application/octet-stream"));
+
+    let headers = [(header::CONTENT_TYPE, producer_content_type)];
+
+    Ok((headers, body).into_response())
 }
 
 async fn channel_subscriber_count(
-    Path(id): Path<Uuid>,
+    Path(id): Path<String>,
     State(state): State<Arc<AppState>>,
 ) -> axum::response::Result<String> {
     let channel_clients = state.channel_clients.lock().await;
@@ -276,7 +309,7 @@ async fn channel_subscriber_count(
 }
 
 async fn channel_close(
-    Path(id): Path<Uuid>,
+    Path(id): Path<String>,
     State(state): State<Arc<AppState>>,
 ) -> axum::response::Result<()> {
     let mut channel_clients = state.channel_clients.lock().await;
@@ -301,7 +334,7 @@ type PubSubClients = Mutex<HashMap<Uuid, broadcast::Sender<PubsubMessage>>>;
 
 type ChannelClients = Mutex<
     HashMap<
-        Uuid,
+        String,
         (
             flume::Sender<(BodyDataStream, HeaderMap, Done)>,
             flume::Receiver<(BodyDataStream, HeaderMap, Done)>,
@@ -316,12 +349,27 @@ struct AppState {
     options: Options,
 }
 
-#[derive(Parser, Debug, Default)]
+#[derive(Parser, Debug)]
 struct Options {
+    /// the port to bind the server to
     #[arg(short, long, env, default_value = "3000")]
     port: u16,
+    /// the maximum request timeout, in seconds
     #[arg(short, long, env)]
     request_timeout: Option<u64>,
+    /// create named channels and pubsubs when they are first requested
+    #[arg(short, long, env, default_value = "true")]
+    autovivify: bool,
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Self {
+            port: 3000,
+            request_timeout: None,
+            autovivify: true,
+        }
+    }
 }
 
 fn app(options: Options) -> axum::Router {
@@ -339,6 +387,7 @@ fn app(options: Options) -> axum::Router {
         .route("/pubsub/{id}/subscribers_count", get(pubsub_count));
 
     let channels_routes = Router::new()
+        .route("/channels", get(channel_index))
         .route("/channels", post(channel_create))
         .route("/channels/{id}", get(channel_subscribe))
         .route("/channels/{id}", post(channel_broadcast))
@@ -701,5 +750,173 @@ mod tests {
         );
         let get_response_text = get_response.text().await.unwrap();
         assert_eq!(get_response_text, test_message);
+    }
+
+    #[tokio::test]
+    async fn channels_autovivify_on_receive() {
+        let options = Options::default();
+
+        let port = get_port();
+
+        let listener = tokio::net::TcpListener::bind(("0.0.0.0", port))
+            .await
+            .unwrap();
+
+        let (_done, done_rx) = Done::new();
+
+        tokio::spawn(async move {
+            axum::serve(listener, app(options))
+                .with_graceful_shutdown(async move { done_rx.await.unwrap() })
+                .await
+                .unwrap();
+        });
+
+        tokio::spawn(async move {
+            reqwest::get(format!(
+                "http://localhost:{port}/channels/it_should_autovivify_on_receive"
+            ))
+            .await
+            .unwrap()
+        });
+
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
+        let ids: Vec<String> = reqwest::get(format!("http://localhost:{port}/channels"))
+            .await
+            .unwrap()
+            .json()
+            .await
+            .unwrap();
+
+        assert_eq!(ids, vec!["it_should_autovivify_on_receive"])
+    }
+
+    #[tokio::test]
+    async fn channels_autovivify_on_publish() {
+        let options = Options::default();
+
+        let port = get_port();
+
+        let listener = tokio::net::TcpListener::bind(("0.0.0.0", port))
+            .await
+            .unwrap();
+
+        let (_done, done_rx) = Done::new();
+
+        tokio::spawn(async move {
+            axum::serve(listener, app(options))
+                .with_graceful_shutdown(async move { done_rx.await.unwrap() })
+                .await
+                .unwrap();
+        });
+
+        tokio::spawn(async move {
+            reqwest::Client::new()
+                .post(format!(
+                    "http://localhost:{port}/channels/it_should_autovivify_on_publish"
+                ))
+                .body("some body")
+                .send()
+                .await
+                .unwrap()
+        });
+
+        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+
+        let ids: Vec<String> = reqwest::get(format!("http://localhost:{port}/channels"))
+            .await
+            .unwrap()
+            .json()
+            .await
+            .unwrap();
+
+        assert_eq!(ids, vec!["it_should_autovivify_on_publish"])
+    }
+
+    #[tokio::test]
+    async fn channels_do_not_autovivify_on_receive_when_disabled() {
+        let options = Options {
+            autovivify: false,
+            ..Default::default()
+        };
+
+        let port = get_port();
+
+        let listener = tokio::net::TcpListener::bind(("0.0.0.0", port))
+            .await
+            .unwrap();
+
+        let (_done, done_rx) = Done::new();
+
+        tokio::spawn(async move {
+            axum::serve(listener, app(options))
+                .with_graceful_shutdown(async move { done_rx.await.unwrap() })
+                .await
+                .unwrap();
+        });
+
+        assert_eq!(
+            reqwest::get(format!(
+                "http://localhost:{port}/channels/it_should_autovivify_on_receive"
+            ))
+            .await
+            .unwrap()
+            .status(),
+            StatusCode::NOT_FOUND
+        );
+
+        let ids: Vec<String> = reqwest::get(format!("http://localhost:{port}/channels"))
+            .await
+            .unwrap()
+            .json()
+            .await
+            .unwrap();
+
+        assert_eq!(ids, Vec::<String>::new())
+    }
+
+    #[tokio::test]
+    async fn channels_do_not_autovivify_on_publish_when_disabled() {
+        let options = Options {
+            autovivify: false,
+            ..Default::default()
+        };
+
+        let port = get_port();
+
+        let listener = tokio::net::TcpListener::bind(("0.0.0.0", port))
+            .await
+            .unwrap();
+
+        let (_done, done_rx) = Done::new();
+
+        tokio::spawn(async move {
+            axum::serve(listener, app(options))
+                .with_graceful_shutdown(async move { done_rx.await.unwrap() })
+                .await
+                .unwrap();
+        });
+
+        assert_eq!(
+            reqwest::Client::new()
+                .post(format!(
+                    "http://localhost:{port}/channels/it_should_autovivify_on_publish"
+                ))
+                .body("some body")
+                .send()
+                .await
+                .unwrap()
+                .status(),
+            StatusCode::NOT_FOUND
+        );
+
+        let ids: Vec<String> = reqwest::get(format!("http://localhost:{port}/channels"))
+            .await
+            .unwrap()
+            .json()
+            .await
+            .unwrap();
+
+        assert_eq!(ids, Vec::<String>::new())
     }
 }
